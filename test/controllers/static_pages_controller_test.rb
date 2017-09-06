45dc2ee596d88ce9965aede5fa7752d3377b155a
jiatao wu <jiataow@student.unimelb.edu.au>
2017-09-06 07:54:30 +0000

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,23 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
+    assert_select "title", "Bibliography Database Research"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
+    assert_select "title", "Help | Bibliography Database Research"
+  end
+
+  test "should get about" do
+    get about_path
+    assert_response :success
+    assert_select "title", "About | Bibliography Database Research"
   end
 
 end
