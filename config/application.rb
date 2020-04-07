e42b821f7f27b808569215b9333e07befcebd85b
jiataow <jessewjt@gmail.com>
2020-04-07 18:29:13 +1000

    config_mongoid

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,9 @@ module BiblioApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-
+    config.generators do |g|
+  		g.orm :mongoid
+	end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
